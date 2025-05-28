3c7e41a07f8d442bb4ef062216fc074ab3a37e5d
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-29 00:32:49 +0600

    added

---
+++ src/app/(commonlayout)/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
+
+
+
+const CommonLayout = ({children} :{children:React.ReactNode}) => {
+    return (
+        <div className="flex flex-col min-h-screen">
+          <Navbar></Navbar>
+          <div className="min-h-screen container mx-auto">
+          {children}
+          </div>
+           <Footer></Footer>
+        </div>
+    );
+};
+
+export default CommonLayout;
