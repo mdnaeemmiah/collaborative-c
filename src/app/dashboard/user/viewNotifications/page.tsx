498119d267a9179636dfa7b94ad84ca06bdb00f5
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-30 02:11:13 +0600

    added

--- src/app/dashboard/user/viewNotifications/page.tsx
+++ src/app/dashboard/user/viewNotifications/page.tsx
@@ -1,6 +1,11 @@
+import { useGetNotificationsQuery } from '@/redux/features/notification/notificationSlice';
+import { useGetUsersQuery } from '@/redux/features/user/userSlice';
 import React from 'react';
 
 const page = () => {
+    const {data}  =useGetNotificationsQuery(undefined)
+    const {}  =useGetUsersQuery(undefined);
+
     return (
         <div>
             <h2>notifications</h2>
